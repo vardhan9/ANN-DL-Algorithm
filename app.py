f77b8d0593dace1424ec56be3ed129d481b3b06a
Raja Marthala <raja.marthala@fliq.fi>
2024-09-23 13:44:39 +0300

    Model trained, predictions and streamlit app created

---
+++ app.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model
+import pickle
+import streamlit as st
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+
+
+## Load trained model, scaler pickle, onehot pickle file
+
+model=load_model('model.h5')
+
+## load the encoder and scaler
+
+with open('label_encoder_gender.pkl', 'rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+with open('onehot_encoder_geo.pkl', 'rb') as file:
+    label_encoder_geo = pickle.load(file)
+
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+#Streamlit app
+
+st.title = ('Customer Churn Prediction')
+
+#User input
+
+geography = st.selectbox('Geography', label_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+# Prepare the input data
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary]
+})
+
+# One hot encode Geography
+geo_encoded = label_encoder_geo.transform([[input_data['Geography']]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=label_encoder_geo.get_feature_names_out(['Geography']))
+
+
+#combine one-hot encoded columns with input data
+
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+#Scaling the input data
+
+input_data_scaled = scaler.transform(input_data)
+
+#Predict churn
+
+prediction = model.predict(input_data_scaled)
+prediction_prob = prediction[0][0]
+
+st.write(f'Churn Probability: {prediction_prob:.2f} * 100,'%'')
+
+if prediction_prob > 0.5:
+    st.write('The customer is likely to churn.')
+else:
+    st.write('The customer is not likely to churn.')
